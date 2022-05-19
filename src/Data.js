f187ce904c71bd0661df0996f8e3034da8f51801
yentlc <98667887+yentlc@users.noreply.github.com>
2022-05-19 13:12:29 -0300

    added the data

---
+++ src/Data.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    title: 'Mount Fuji',
+    location: 'Japan',
+    googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description:
+      'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+    imageUrl: 'https://source.unsplash.com/WLxQvbMyfas',
+  },
+  {
+    title: 'Cerro Galán',
+    location: 'Argentina',
+    googleMapsUrl: 'https://goo.gl/maps/xNcFWBK7RrnEZBm8A',
+    startDate: '05 Feb, 2019',
+    endDate: '15 Feb, 2019',
+    description:
+      'The Cerro Galán is one of the largest exposed calderas in the world and forms part of the Central Volcanic Zone of the Andes, one of the three volcanic belts found in South America.',
+    imageUrl: 'https://unsplash.com/photos/nmEkUBE0RJM',
+  },
+  {
+    title: 'Casco Antiguo',
+    location: 'Panamá',
+    googleMapsUrl: 'https://goo.gl/maps/9tBEq86GiXA4WLK79',
+    startDate: '20 Mar, 2020',
+    endDate: '30 May, 2020',
+    description:
+      'Casco Antiguo is the historic district of Panama City. Completed and settled in 1673, it was built following the near-total destruction of the original Panamá city, Panamá Viejo in 1671, when the latter was attacked by pirates. It was designated a World Heritage Site in 1997.',
+    imageUrl: 'https://unsplash.com/photos/Ab9Jw_Wl374',
+  },
+  {
+    title: 'Barcelona',
+    location: 'Spain',
+    googleMapsUrl: 'https://goo.gl/maps/Msh9LbBjfNmgbT3m7',
+    startDate: '20 Nov, 2022',
+    endDate: '11 Mar, 2023',
+    description:
+      'Barcelona is the capital and largest city of the autonomous community of Catalonia, as well as the second most populous municipality of Spain.',
+    imageUrl: 'https://unsplash.com/photos/RoM8ocpgBkI',
+  },
+  {
+    title: 'Lisbon',
+    location: 'Portugal',
+    googleMapsUrl: 'https://goo.gl/maps/RntMnChTYLWco6DU9',
+    startDate: '12 Mar, 2023',
+    endDate: '23 May, 2023',
+    description:
+      'Lisbon is recognised as an alpha-level global city because of its importance in finance, commerce, fashion, media, entertainment, arts, international trade, education and tourism.',
+    imageUrl: 'https://unsplash.com/photos/rYVmXecm64Q',
+  },
+  {
+    title: 'Bali',
+    location: 'Indonesia',
+    googleMapsUrl: 'https://goo.gl/maps/TUpM5KAJ1oTdfwps8',
+    startDate: '20 Dic, 2023',
+    endDate: '05 Jan, 2024',
+    description:
+      'Bali is renowned for its highly developed arts, including traditional and modern dance, sculpture, painting, leather, metalworking, and music. The Indonesian International Film Festival is held every year in Bali.',
+    imageUrl: 'https://unsplash.com/photos/U_i6h9Y50wQ',
+  },
+  {
+    title: 'Bay East Garden',
+    location: 'Singapore',
+    googleMapsUrl: 'https://goo.gl/maps/drS9GW3Wgu8XBqnXA',
+    startDate: '01 Jun, 2024',
+    endDate: '01 Jul, 2024',
+    description:
+      " The country's territory is composed of one main island, 63 satellite islands and islets, and one outlying islet, the combined area of which has increased by 25% since the country's independence as a result of extensive land reclamation projects. It has the third greatest population density in the world. With a multicultural population and recognising the need to respect cultural identities of the major ethnic groups within the nation, Singapore has four official languages; English, Malay, Mandarin, and Tamil.",
+    imageUrl: 'https://unsplash.com/photos/OZ2rS2zCjNo',
+  },
+];
